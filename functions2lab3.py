2232ced3d7d5974b9fd9e5a94991e95f87496507
AdilYergazy <156777386+AdilYergazy@users.noreply.github.com>
2024-01-30 16:54:57 +0600

    Update functions2lab3.py

--- functions2lab3.py
+++ functions2lab3.py
@@ -1,9 +1,93 @@
 # 1
+# Dictionary of movies
+
+movies = [
+    {
+        "name": "Usual Suspects",
+        "imdb": 7.0,
+        "category": "Thriller"
+    },
+    {
+        "name": "Hitman",
+        "imdb": 6.3,
+        "category": "Action"
+    },
+    {
+        "name": "Dark Knight",
+        "imdb": 9.0,
+        "category": "Adventure"
+    },
+    {
+        "name": "The Help",
+        "imdb": 8.0,
+        "category": "Drama"
+    },
+    {
+        "name": "The Choice",
+        "imdb": 6.2,
+        "category": "Romance"
+    },
+    {
+        "name": "Colonia",
+        "imdb": 7.4,
+        "category": "Romance"
+    },
+    {
+        "name": "Love",
+        "imdb": 6.0,
+        "category": "Romance"
+    },
+    {
+        "name": "Bride Wars",
+        "imdb": 5.4,
+        "category": "Romance"
+    },
+    {
+        "name": "AlphaJet",
+        "imdb": 3.2,
+        "category": "War"
+    },
+    {
+        "name": "Ringing Crime",
+        "imdb": 4.0,
+        "category": "Crime"
+    },
+    {
+        "name": "Joking muck",
+        "imdb": 7.2,
+        "category": "Comedy"
+    },
+    {
+        "name": "What is the name",
+        "imdb": 9.2,
+        "category": "Suspense"
+    },
+    {
+        "name": "Detective",
+        "imdb": 7.0,
+        "category": "Suspense"
+    },
+    {
+        "name": "Exam",
+        "imdb": 4.2,
+        "category": "Thriller"
+    },
+    {
+        "name": "We Two",
+        "imdb": 7.2,
+        "category": "Romance"
+    }
+]
+
+
 def rate(movie):
     if movie['imdb'] > 5.5:
         return True
+
+
 for i in movies:
     print(rate(i))
+
 
 # 2
 def rate2(movies):
@@ -12,6 +96,8 @@ def rate2(movies):
         if i['imdb'] > 5.5:
             mov.append(i['name'])
     return mov
+
+
 # 3
 def rat3(category):
     mov = []
@@ -19,6 +105,8 @@ def rat3(category):
         if i['category'] == category:
             mov.append(i['name'])
     return mov
+
+
 # 4
 def rate4(movies):
     sum1 = 0
@@ -28,6 +116,8 @@ def rate4(movies):
         count += 1
         sum1 += i['imdb']
     return sum1 / count
+
+
 # 5
 def rat3(category):
     mov = []
